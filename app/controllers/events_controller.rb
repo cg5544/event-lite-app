57c4c586bbad42d850c076d9d5804971199954dc
Cai Gao <cai.gao@icloud.com>
2020-11-29 00:04:11 -0500

    Handling form submission when user create a new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,19 @@ class EventsController < ApplicationController
   def index
     @events = Event.order('start_datetime ASC')
   end
+
+  def create
+    @event = Event.new(event_params)
+    @event.start_datetime = event_params[:start_datetime]&.to_datetime
+    if @event.save
+      render json: @event
+    else
+      render json: @event.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:title, :start_datetime, :location)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'events#index'
+  resources :events
 end
